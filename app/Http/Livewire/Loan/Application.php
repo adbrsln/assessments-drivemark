df3c71b84767f5098ebbe37fd9550e085d6bcf31
Muhammad Adib Ahmad Roslan <adbroslan@gmail.com>
2020-09-24 13:39:19 +0800

    added ui and layouts

---
+++ app/Http/Livewire/Loan/Application.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Loan;
+
+use Livewire\Component;
+
+class Application extends Component
+{
+    public function render()
+    {
+        return view('livewire.loan.application');
+    }
+}
