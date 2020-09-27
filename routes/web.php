4fa618a04c53e17cf910e5256cd09a5e84ed93e7
Muhammad Adib Ahmad Roslan <adbroslan@gmail.com>
2020-09-28 02:13:17 +0800

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
-// Route::get('/dashboard' , App\Http\Livewire\Loan\Listing::class);
+// Route::get('/', 'welcome')->name('home');
+Route::redirect('/', 'login', 301);
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
         ->name('login');
@@ -52,4 +52,11 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', LogoutController::class)
         ->name('logout');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', \App\Http\Livewire\Loan\Listing::class)->name('application.list');
+    Route::get('/create', \App\Http\Livewire\Loan\Create::class)->name('application.create');
+    Route::get('/{uuid}', \App\Http\Livewire\Loan\Show::class)->name('application.show');
 });
